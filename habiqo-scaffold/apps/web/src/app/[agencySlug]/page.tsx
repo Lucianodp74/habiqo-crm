07d650793354e868bea5b95528ce5d1188ab1191
Luciano <lucianodelpriore74@gmail.com>
2026-05-23 23:28:09 +0200

    feat: aggiungi RenovationShowcase alla homepage HabitaMi

--- habiqo-scaffold/apps/web/src/app/[agencySlug]/page.tsx
+++ habiqo-scaffold/apps/web/src/app/[agencySlug]/page.tsx
@@ -3,6 +3,7 @@ import { AgencyHero } from "@/components/habita/agency-hero";
 import { AgencyFeaturedProperties } from "@/components/habita/agency-featured-properties";
 import { AgencyTrustSignals } from "@/components/habita/agency-trust-signals";
 import { AgencyAbout } from "@/components/habita/agency-about";
+import { RenovationShowcase } from "@/components/habita/renovation-showcase";
 import { AgencyContact } from "@/components/habita/agency-contact";
 
 type Params = Promise<{ agencySlug: string }>;
@@ -21,6 +22,7 @@ export default async function HabitaAgencyHomePage({
       <AgencyHero agency={agency} />
       <AgencyFeaturedProperties agency={agency} />
       <AgencyTrustSignals />
+      <RenovationShowcase agencyId={agency.id} agencyName={agency.name} />
       <AgencyAbout agency={agency} />
       <AgencyContact agency={agency} />
     </>
